3de98503259b47bd908bc10b4a6b665d6bbbda33
Chris Voitova <voitova@teamharbour.com>
2020-12-14 19:31:05 +0200

    added user render

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const url = "https://randomuser.me/api/?results=16";
+const usersContainer = document.querySelector(".users-container");
+console.log(usersContainer);
 
 async function getUsers() {
   try {
@@ -13,12 +15,46 @@ async function getUsers() {
 async function app() {
   const initUsers = await getUsers();
   const users = initUsers.slice();
-  console.log("init: ", initUsers);
-  //   console.log(sortByAge(users));
-  //   console.log(sortByName(users));
-  //   console.log(filterByGender(users, "female"));
+  console.log(users);
+  renderUsers(users);
   return users;
 }
+
+const userTemplate = (photo, name, email, gender, age) => {
+  return `<div class="user">
+		 <img
+			class="user__photo"
+			src=${photo}
+			alt="user-photo"
+		 />
+		 <div class="user__info">
+			<h3 class="user__name">${name}</h3>
+			<a class="user__email" href="mailto:${email}">
+			 ${email}
+			</a>
+			<span class="user__gender">gender: ${gender}</span>
+			<span class="user__age">age: ${age}</span>
+		 </div>
+	  </div>`;
+};
+
+const renderUsers = (users) => {
+  let renderedUsers = "";
+  users.forEach(
+    ({
+      picture: { medium: photo },
+      name: { first, last },
+      email,
+      gender,
+      dob: { age },
+    }) => {
+      const name = `${first} ${last}`;
+      renderedUsers += userTemplate(photo, name, email, gender, age);
+    }
+  );
+  usersContainer.innerHTML += renderedUsers;
+};
+
 function sortByAge(users) {
   return users.sort((a, b) => a.dob.age - b.dob.age);
 }
